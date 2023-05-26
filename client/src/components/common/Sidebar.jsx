8a8b6b7dee710913b4880400dd59927eda1f7303
Tanya <xb22162@bristol.ac.uk>
2023-05-26 22:40:06 +0100

    Sidebar + Home page initial

--- client/src/components/common/Sidebar.jsx
+++ client/src/components/common/Sidebar.jsx
@@ -1,8 +1,97 @@
-import React from 'react'
+import {useSelector} from 'react-redux'
+import {useNavigate} from 'react-router-dom'
+import {Drawer,List,ListItem,Typography,IconButton,Box} from '@mui/material'
+import LogoutOutlinedIcon from '@mui/icons-material/LogoutOutlined'
+import AddBoxOutlinedIcon from '@mui/icons-material/AddBoxOutlined'
+import images from '../../../public/index'
 
 const Sidebar = () => {
+const navigate =useNavigate()
+const user =useSelector((state)=>state.user.value)
+const sidebarWidth = 250
+
+const logout =()=>{
+  localStorage.removeItem('token')
+  navigate('/login')
+}
+
   return (
-    <div>Sidebar</div>
+    <Drawer
+      container={window.document.body}
+      variant='permanent'
+      open={true}
+      sx={{
+        width:sidebarWidth,
+        height:'100%',
+        '& > div':{borderRight:'none'}
+      }}
+    >
+        <List
+          disablePadding
+          sx={{
+             width:sidebarWidth,
+             height:'100vh',
+             backgroundColor:images.colors.secondary
+          }}
+        >
+            <ListItem>
+                <Box sx={{
+                    width:'100%',
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'space-between'
+                }}> 
+
+                    <Typography variant='body2' fontWeight='700'>
+                      {user.username}
+                    </Typography>
+
+                    <IconButton onClick={logout}>
+                      <LogoutOutlinedIcon fontSize='small'/>
+                    </IconButton>
+                </Box>
+            </ListItem>
+            
+            <Box sx={{paddingTop:'10px'}}>
+                <ListItem>
+                <Box sx={{
+                    width:'100%',
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'space-between'
+                }}> 
+
+                    <Typography variant='body2' fontWeight='700'>
+                      Marked
+                    </Typography>
+                </Box>
+                </ListItem>
+             </Box>
+          
+             <Box sx={{paddingTop:'10px'}}>
+                <ListItem>
+                    <Box sx={{
+                    width:'100%',
+                    display:'flex',
+                    alignItems:'center',
+                    justifyContent:'space-between'
+                }}> 
+
+                    <Typography variant='body2' fontWeight='700'>
+                      Diary
+                    </Typography>
+                    <IconButton>
+                        <AddBoxOutlinedIcon />
+                    </IconButton>
+                    
+                    
+                    </Box>
+                </ListItem>
+             </Box>
+          
+
+        </List>
+    </Drawer>
   )
 }
 
--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import{Box} from '@mui/material'
+import LoadingButton from '@mui/lab/LoadingButton'
 
 const Home = () => {
+  const createBoard =()=>{
+    
+  }
+
+
+
   return (
-    <div>Home</div>
+      <Box sx={{
+        height:'100%',
+        display:'flex',
+        alignItems: 'center',
+        justifyContent:'center',
+        
+      }}> 
+          <LoadingButton
+            variant='outlined'
+            color='success'
+            onClick={createBoard}
+            sx={{
+              
+              position:'absolute',
+              top: '50%', // 垂直居中，距离顶部偏移 50%
+              left: '50%', // 水平居中，距离左侧偏移 50%  
+            }}
+          >
+            Click here to create your first board
+          </LoadingButton>
+      </Box>
   )
 }
 
